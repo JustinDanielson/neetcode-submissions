f20e6ecbd84756cbce6261293bf39b68aeafaa6a
Justin Danielson <danielson.justin@gmail.com>
2026-08-19 14:42:39 -0500

    Add: binary-tree-right-side-view - submission-4

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-4.py
@@ -0,0 +1,13 @@
+# Definition for a binary tree node.
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        def post_order(root: Optional[TreeNode], level = 1): 
+            if not root:
+                return
+            if len(result) < level:
+                result.append(root.val)
+            post_order(root.right, level + 1)
+            post_order(root.left, level + 1)
+        post_order(root)
+        return result
